0d83e46f981a55cef8fb7677956166f0303d7efe
Miras <mirazhhi.1337@gmail.com>
2023-04-25 18:25:41 +0600

    feat: Rabbit mq

--- src/Layers/Infrastructure/MessageBroker/RabbitMQ/Connection.php
+++ src/Layers/Infrastructure/MessageBroker/RabbitMQ/Connection.php
@@ -1,8 +1,21 @@
 <?php
-
 namespace Centras\Layers\Infrastructure\MessageBroker\RabbitMQ;
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class Connection
 {
-
+    /**
+     * @return AMQPStreamConnection
+     * @throws \Exception
+     */
+    public function connect(): AMQPStreamConnection
+    {
+        return new AMQPStreamConnection(
+            'rabbitmq',
+            5672,
+            'guest',
+            'guest'
+        );
+    }
 }
